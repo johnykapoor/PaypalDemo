7a39dad5df5133dfe3540ec1940659e501ad7214
johnykapoor <kapoor.johny11@gmail.com>
2017-07-12 15:07:54 +0530

    dev2 login file push comiit

---
+++ app/src/main/java/com/technotaau/paypaldemo/Login_deve_two.java
@@ -0,0 +1,13 @@
+package com.technotaau.paypaldemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login_deve_two extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_deve_two);
+    }
+}
